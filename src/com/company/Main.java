33535bcc3f0cd88bfd6a4da1a444fdcb4fcbd16d
Magnus <51696488+Magnusahl@users.noreply.github.com>
2019-12-10 14:16:38 +0100

    Added game class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,17 @@ package com.company;
 
 public class Main {
     public static void main(String[]args) {
+        Player p1 = new Player("Player", "X");
+        Player p2 = new Player("Player", "O");
 
-
-        Field field1 = new Field('3','3','3');
+        Field field1 = new Field('3','3','1');
         field1.printfield();
 
+        while(true) {
+            Game gamea = new Game(1, true);
+            gamea.gameRun(p1);
+            gamea.gameRun(p2);
+        }
 
     }
 
